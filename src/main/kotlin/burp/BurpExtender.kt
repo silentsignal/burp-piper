5f9097d6ab0d5be23c30f4920a87021b58220afb
András Veres-Szentkirályi <vsza@vsza.hu>
2019-01-07 21:57:08 +0100

    extracted Kotlin file Editors.kt

--- src/main/kotlin/burp/BurpExtender.kt
+++ src/main/kotlin/burp/BurpExtender.kt
@@ -22,7 +22,6 @@ import com.google.protobuf.ByteString
 import com.redpois0n.terminal.JTerminal
 import org.snakeyaml.engine.v1.api.Dump
 import org.snakeyaml.engine.v1.api.DumpSettingsBuilder
-import java.awt.Component
 import java.io.File
 import java.io.InputStream
 import java.util.*
@@ -71,104 +70,6 @@ enum class RequestResponse {
     abstract fun getMessage(rr: IHttpRequestResponse): ByteArray?
     abstract fun getBodyOffset(data: ByteArray, helpers: IExtensionHelpers): Int
     abstract fun getHeaders(data: ByteArray, helpers: IExtensionHelpers): List<String>
-}
-
-val IS_REQUEST_MAP = mapOf(
-        true to RequestResponse.REQUEST,
-        false to RequestResponse.RESPONSE
-)
-
-class TerminalEditor(private val tool: Piper.MessageViewer, private val helpers: IExtensionHelpers,
-                     private val callbacks: IBurpExtenderCallbacks) : IMessageEditorTab {
-    private var msg: ByteArray? = null
-    private val terminal = JTerminal()
-    private val scrollPane = JScrollPane()
-
-    init {
-        scrollPane.setViewportView(terminal)
-    }
-
-    override fun isEnabled(content: ByteArray?, isRequest: Boolean): Boolean {
-        if (content == null) return false
-        if (!tool.common.hasFilter()) return true
-        val payload = transformContent(content, isRequest)
-        return payload.isNotEmpty() && tool.common.filter.matches(payload, helpers)
-    }
-
-    private fun transformContent(content: ByteArray, isRequest: Boolean): ByteArray {
-        if (tool.common.cmd.passHeaders) return content
-        val rr = IS_REQUEST_MAP[isRequest]!!
-        val bo = rr.getBodyOffset(content, helpers)
-        return content.copyOfRange(bo, content.size)
-    }
-
-    override fun getMessage(): ByteArray? = msg
-    override fun isModified(): Boolean = false
-    override fun getTabCaption(): String = tool.common.name
-    override fun getSelectedData(): ByteArray = helpers.stringToBytes(terminal.selectedText)
-    override fun getUiComponent(): Component = terminal
-
-    override fun setMessage(content: ByteArray?, isRequest: Boolean) {
-        msg = content
-        if (content == null) return
-        terminal.text = ""
-        thread {
-            tool.common.cmd.execute(listOf(transformContent(content, isRequest))).processOutput { process ->
-                for (stream in arrayOf(process.inputStream, process.errorStream)) {
-                    thread {
-                        val reader = stream.bufferedReader()
-                        while (true) {
-                            val line = reader.readLine() ?: break
-                            terminal.append("$line\n")
-                        }
-                    }.start()
-                }
-            }
-        }.start()
-    }
-}
-
-class TextEditor(private val tool: Piper.MessageViewer, private val helpers: IExtensionHelpers,
-                 private val callbacks: IBurpExtenderCallbacks) : IMessageEditorTab {
-    private var msg: ByteArray? = null
-    private val editor = callbacks.createTextEditor()
-
-    init {
-        editor.setEditable(false)
-    }
-
-    override fun isEnabled(content: ByteArray?, isRequest: Boolean): Boolean {
-        if (content == null) return false
-        if (!tool.common.hasFilter()) return true
-        val payload = transformContent(content, isRequest)
-        return payload.content.isNotEmpty() && tool.common.filter.matches(payload, helpers)
-    }
-
-    private fun transformContent(content: ByteArray, isRequest: Boolean): MessageInfo {
-        val rr = IS_REQUEST_MAP[isRequest]!!
-        var payload = if (tool.common.cmd.passHeaders) content
-            else content.copyOfRange(rr.getBodyOffset(content, helpers), content.size)
-        return MessageInfo(payload, helpers.bytesToString(payload), rr.getHeaders(content, helpers))
-    }
-
-    override fun getMessage(): ByteArray? = msg
-    override fun isModified(): Boolean = false
-    override fun getTabCaption(): String = tool.common.name
-    override fun getSelectedData(): ByteArray = editor.selectedText
-    override fun getUiComponent(): Component = editor.component
-
-    override fun setMessage(content: ByteArray?, isRequest: Boolean) {
-        msg = content
-        if (content == null) return
-        thread {
-            tool.common.cmd.execute(listOf(transformContent(content, isRequest).content)).processOutput { process ->
-                process.inputStream.use {
-                    val bytes = it.readBytes()
-                    SwingUtilities.invokeLater { editor.text = bytes }
-                }
-            }
-        }.start()
-    }
 }
 
 class BurpExtender : IBurpExtender {
